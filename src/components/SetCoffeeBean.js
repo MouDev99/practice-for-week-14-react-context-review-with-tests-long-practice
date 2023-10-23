e24ea6dbdb6cf06f1d639a75cb09f485256a57b5
moud <haibou177@gmail.com>
2023-10-23 10:41:25 +0000

    done

--- src/components/SetCoffeeBean.js
+++ src/components/SetCoffeeBean.js
@@ -1,9 +1,15 @@
+import { useCoffee } from '../context/CoffeeContext';
+
 const SetCoffeeBean = ({ coffeeBeans }) => {
+  const { coffeeBean, setCoffeeBeanId } = useCoffee();
+
   return (
     <div className="set-coffee-bean">
       <h2>Select a Coffee Bean</h2>
       <select
         name="coffee-bean"
+        onChange={(e) => setCoffeeBeanId(e.target.value)}
+        defaultValue={coffeeBean.id}
       >
         {coffeeBeans.map(bean => (
           <option
